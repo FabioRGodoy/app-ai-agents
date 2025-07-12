75a35fa88c7b20700e586d7c236e20651df8f3a8
Fabio <fabiogodoy955@icloud.com>
2025-07-12 16:38:12 -0300

    Added Docker Compose configuration for the PostgreSQL database service and implements the initial database package structure with migrations and tables allowed. Update dependencies and references in the project.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { db } from "@repo/database";
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: "PRISMA",
+      useValue: db,
+    }
+  ],
 })
 export class AppModule { }
