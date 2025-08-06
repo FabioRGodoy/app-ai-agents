daa89945f17502ef39cf42c9b3e8863cd8fb9a96
Fabio Godoy <fabio@sync.com.br>
2025-08-05 21:09:54 -0300

    feat(core): upgrade tailwind

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 export default function RootPage() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-red-500">
-      <h1 className="text-4xl font-bold text-blue-500">Hello World</h1>
+      <h1 className="text-4xl font-bold text-green-500">Hello World</h1>
     </main>
   );
 }
